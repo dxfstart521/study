12f85bf7b5027a4ec0d3c2cfdc1be0999e2f87a5
dingxiaofei <dingxiaofei@didichuxing.com>
2017-09-05 23:27:21 +0800

    update es6

---
+++ es6/1.js
@@ -0,0 +1,51 @@
+一.es6
+
+1.Object.assign(obj, newObj)
+2.函数默认参数
+function hello(text = 'hello'){
+	return text;
+}
+3.字符串模范
+var name= 'dingding';
+var str = `my name is ${name}`;
+4.解构赋值
+5.箭头函数
+6.Set 和 Map
+7.异步操作
+8.类和对象
+9.模块化
+
+
+二、es6的项目构建
+1.基础构建
+业务逻辑：页面和交互
+自动构建： 编译和辅助（自动刷新，文件合并，资源压缩）
+服务接口：数据和接口
+2.任务自动化(glup)
+glup 的中文网
+没听太懂，自己在查资料
+glup 的作用，
+3.编译工具（babel, webpack）
+4.代码实现
+创建一个es6的前端工程
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
